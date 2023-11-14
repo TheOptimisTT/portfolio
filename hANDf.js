44f5262dc5700b8891ec0681d6adc49357429436
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2023-11-14 21:02:05 +0200

    First Commit

---
+++ hANDf.js
@@ -0,0 +1,37 @@
+//Header
+document.getElementById("header").innerHTML =` <div class="container">
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+  <a  class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+    <span class="fs-4">TheOpTimisTT</span>
+  </a>
+
+  <ul class="nav nav-pills">
+    <li class="nav-item"><a href="#hero" class="nav-link active" aria-current="page">Home</a></li>
+    <li class="nav-item"><a href="#projects" class="nav-link">Projects</a></li>
+    <li class="nav-item"><a href="#games" class="nav-link">Games</a></li>
+    <li class="nav-item"><a href="#apps" class="nav-link">Apps</a></li>
+    <li class="nav-item"><a href="#contact" class="nav-link">Contacts</a></li>
+  </ul>
+</header>
+</div>`;
+
+//Footer
+document.getElementById("footer").innerHTML = `
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 text-body-secondary">© 2023 TheOpTimisTT</p>
+        
+        <a  class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#hero" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="#projects" class="nav-link px-2 text-body-secondary">Projects</a></li>
+            <li class="nav-item"><a href="#games" class="nav-link px-2 text-body-secondary">Games</a></li>
+            <li class="nav-item"><a href="#apps" class="nav-link px-2 text-body-secondary">Apps</a></li>
+            <li class="nav-item"><a href="#contact" class="nav-link px-2 text-body-secondary">Contacts</a></li>
+        </ul>
+    </footer>
+</div>`;
+
